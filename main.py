6230263df5428e097af7c29519de69d3919fe12c
CoBCem-kyky <70327355+CoBCem-kyky@users.noreply.github.com>
2021-05-21 15:12:13 +0300

    # Тип данных Множества (Set)

--- main.py
+++ main.py
@@ -458,6 +458,7 @@ print(my_set)
 my_list = list(my_set)
 print(my_list)
 '''
+'''
 my_list = [1, 1, 2, 5, 10, 10, 10]
 my_set = set(my_list)
 summ = 0
@@ -466,3 +467,19 @@ for i in my_set:
 print(summ)
 
 print(sum(set(my_list)))
+'''
+# Домашка 1
+'''
+def func(first, second):
+    if len(second) > len(first):
+        return False
+
+    for el in second:
+        if el not in first:
+            return False
+    return True
+a = {1, 2, 3, 4, 5, 6}
+b = [3, 1]
+print(func(a, b))
+'''
+# ООП, Классы и объекты
